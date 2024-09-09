3bf189a9439339b56d650011e437925f3eef0276
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-09 10:54:17 -0700

    102-magic_calculation

---
+++ 0x00-python-hello_world/test_magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from 102-magic_calculation import magic_calculation
+
+# Test cases
+print(magic_calculation(2, 3))
+print(magic_calculation(5, 2))
+print(magic_calculation(1, 10))
+print(magic_calculation(3, 3))
