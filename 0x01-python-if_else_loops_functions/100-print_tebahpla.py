d974be9dd34d4f5cd79805d07dce9b3bbce9c711
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-10 13:03:34 -0700

    100-print_tebahpla, 102-magic_calculation

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-def print_tebahpla():
-    for i in range(25, -1, -1):
-        char = chr(122 - i) if i % 2 == 0 else chr(90 - i)
-        print("{}".format(char), end="")
-    print()  # Print a newline at the end
+for i in range(122, 96, -1):
+    print("{}".format(chr(i) if i % 2 == 0 
+        else chr(i - 32)), end="")
