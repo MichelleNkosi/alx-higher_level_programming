583f914a20a8a72f29f5b4a86f1e59f11325adee
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-10 08:35:06 -0700

    100-print_tebahpla, 101-remove_char_at

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,5 @@
-#!/usr/bin/python3
-
 def print_tebahpla():
     for i in range(25, -1, -1):
-        if i % 2 == 0:
-            print(chr(122 - i), end="")
-        else:
-            print(chr(90 - (i - 1)), end="")
-
-print_tebahpla()
+        char = chr(122 - i) if i % 2 == 0 else chr(90 - i)
+        print("{}".format(char), end="")
+    print()  # Print a newline at the end
