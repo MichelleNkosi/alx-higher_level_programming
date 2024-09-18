eea7eb86b05be0623ba8e68578cde447fac3c1b3
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-09-18 08:29:06 -0700

    7-update_dictionary, 8-simple_delete, 9-multiply_by_2, 10-best_score, 11-multiply_list_map, 12-roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    # Define Roman numeral values
+    roman_values = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+        'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        if char not in roman_values:
+            return 0  # Invalid character in the Roman numeral string
+
+        value = roman_values[char]
+
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+
+        prev_value = value
+
+    return total
