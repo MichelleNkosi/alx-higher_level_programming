46fce12132a4304d7af1a4be4b55b2eaee419ff1
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-11-19 09:47:28 -0800

    7-insert_value 8-count_89 9-full_creation

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Task: Display the number of records with id = 89 in the table first_table (database hbtn_0c_0)
+-- The database name will be passed as an argument to the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
+
---
+++ 0x0D-SQL_introduction/create_database.sql
@@ -0,0 +1,5 @@
+-- Task: Create the database hbtn_0c_0 if it doesn't already exist
+-- The database will be created in the MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
