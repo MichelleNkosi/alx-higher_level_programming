95d2c758fe5a8b3e3c6cb1c459011895b0b1f358
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-11-19 09:35:47 -0800

    5-full_table 6-list_values

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Task: Print the full description of the table first_table from the current database
+-- The database name will be passed as an argument of the mysql command
+
+SHOW CREATE TABLE first_table;
+
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- Task: List all rows of the table first_table from the current database
+-- All fields should be printed
+-- The database name will be passed as an argument to the mysql command
+
+SELECT * FROM first_table;
+
